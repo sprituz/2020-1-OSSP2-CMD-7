fbf4610c180846f52991ec3efb9dd3418c4777c7
dayeon <dayeon2768@daum.net>
2020-05-30 16:04:24 +0900

    makefile 만듬

---
+++ opencvtest/CMakeFiles/LaneDetection1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dayeon/2020-1-OSSP2-CMD-7/opencvtest/facialLandmarkDetection.cpp" "/Users/dayeon/2020-1-OSSP2-CMD-7/opencvtest/CMakeFiles/LaneDetection1.dir/facialLandmarkDetection.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.3.0/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
